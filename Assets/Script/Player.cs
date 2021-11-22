f3d1f420725f30ee43265fd1af81322960f62fb9
itcksgud <kingcrown20200@gmail.com>
2021-11-23 03:09:05 +0900

    end

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,8 +7,11 @@ public class Player : MonoBehaviour
     public float speed;
     float hAxis;
     float vAxis;
-    bool wDown;
-
+    bool wUp;
+    bool xDown;
+    bool cDown;
+    bool isBed;
+    bool isBad;
     Vector3 moveVec;
 
     Animator anim;
@@ -21,20 +24,63 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+        Bed();
+        Bad();
+    }
+
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
-        wDown = Input.GetButton("Walk");
+        wUp = Input.GetButton("Run");
+        xDown = Input.GetButtonDown("XXX");
+        cDown = Input.GetButtonDown("CCC");
+    }
+    void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-        if (wDown)
-            transform.position += moveVec * speed * 0.3f * Time.deltaTime;
+        if (wUp)
+            transform.position += moveVec * speed * 1.5f * Time.deltaTime;
         else
             transform.position += moveVec * speed * Time.deltaTime;
 
-        anim.SetBool("isRun", moveVec != Vector3.zero);
-        anim.SetBool("isWalk", wDown);
-
-
+        anim.SetBool("isWalk", moveVec != Vector3.zero);
+        anim.SetBool("isRun", wUp);
+    }
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec);
+    }
+    void Bed()
+    {
+        if (!isBed && xDown)
+        {
+            anim.SetTrigger("doBed");
+            isBed = true;
+
+            Invoke("BedOut", 0.5f);
+        }
+    }
+    void BedOut()
+    {
+        isBed = false;
+    }
+    void Bad()
+    {
+        if (!isBad && cDown)
+        {
+            anim.SetTrigger("doBad");
+            isBad = true;
+
+            Invoke("BadOut", 0.1f);
+        }
+    }
+    void BadOut()
+    {
+        isBad = false;
     }
 }
