0b7e9f82c0bff0b969fd061a0da766f0bc59f03c
LeeHann <0104ihn@naver.com>
2021-11-29 12:10:18 +0900

    맵 정리

--- Assets/02_Scripts/Follow.cs
+++ Assets/02_Scripts/Follow.cs
@@ -6,6 +6,8 @@ public class Follow : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
+
+    public float rotateSpeed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +18,14 @@ public class Follow : MonoBehaviour
     void Update()
     {
         transform.position = target.position + offset;
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 rot = transform.rotation.eulerAngles; // 현재 카메라의 각도를 Vector3로 반환
+            rot.y += Input.GetAxis("Mouse X") * rotateSpeed; // 마우스 X 위치 * 회전 스피드
+            rot.x += -1 * Input.GetAxis("Mouse Y") * rotateSpeed; // 마우스 Y 위치 * 회전 스피드
+            Quaternion q = Quaternion.Euler(rot); // Quaternion으로 변환
+            q.z = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, 2f); // 자연스럽게 회전
+        }
     }
 }
